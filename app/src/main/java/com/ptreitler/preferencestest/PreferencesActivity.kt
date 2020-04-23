4f717cc9c0a2a0c459d37972d56d38eb87112a40
Peter Treitler <peter.treitler@gmail.com>
2020-04-23 12:53:28 +0200

    Update title when navigating

--- app/src/main/java/com/ptreitler/preferencestest/PreferencesActivity.kt
+++ app/src/main/java/com/ptreitler/preferencestest/PreferencesActivity.kt
@@ -1,10 +1,12 @@
 package com.ptreitler.preferencestest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragment
 import androidx.preference.PreferenceFragmentCompat
+
+private const val TITLE_TAG: String = "title"
 
 class PreferencesActivity : AppCompatActivity(),
     PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
@@ -12,6 +14,25 @@ class PreferencesActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+
+        if (savedInstanceState != null) {
+            title = savedInstanceState.getCharSequence(TITLE_TAG)
+        }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                setTitle(R.string.preferences_main_title)
+            }
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Save current activity title so we can set it again after a configuration change
+        outState.putCharSequence(TITLE_TAG, title)
     }
 
     override fun onPreferenceStartFragment(
@@ -31,6 +52,15 @@ class PreferencesActivity : AppCompatActivity(),
             .replace(R.id.preferences_fragment, fragment)
             .addToBackStack(null)
             .commit()
+
+        title = pref.title
+
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            true
+        } else super.onOptionsItemSelected(item)
 }
